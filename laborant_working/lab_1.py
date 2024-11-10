de4c0b58cea70ff2a241b74410de2e527350ad13
tikhi <tikhiy2006@inbox.ru>
2024-11-10 20:55:10 +0900

    create new file

--- laborant_working/lab_1.py
+++ laborant_working/lab_1.py
@@ -1,16 +1,17 @@
 def main():
-
-#1
+    # 1.
     movie = input("Введите название фильма: ")
     cinema = input("Введите название кинотеатра: ")
     time = input("Введите время сеанса: ")
     print(f'Билет на "{movie}" в "{cinema}" на {time} забронирован.')
-#2
+
+    # 2.
     salary = int(input("Зарплата за месяц: \n>>> "))
     opening_hours = int(input("Количество отработанных в выходные часов: \n>>> "))
     prize = salary * 0.01 * opening_hours
     print(f"Размер премии: {prize}")
-#3
+
+    # 3.
     n = int(input('Введите сумму:\n>>> '))
     thousands = n // 1000
     hundreds = (n % 1000) // 100
@@ -20,34 +21,37 @@ def main():
     print(tens, '- по 10р')
     print(hundreds, '- по 100р')
     print(thousands, '- по 1000р')
-#4
+
+    # 4.
     review = input("Оцените развлекательный комплекс: \n>>> ")
     keywords = ["весело", "увлекательно", "развлечения"]
     result = [review.find(keyword) for keyword in keywords]
     print("Результат анализа:", *result, sep=' ')
-#5
+
+    # 5.
     text = input('Введите слово: ')
     middle_index = len(text) // 2
     if len(text) % 2 == 0:
         print(f'Средние буквы: {text[middle_index - 1]}')
     else:
         print(f'Средняя буква: {text[middle_index]}')
-#6
+
+    # 6.
     feedback = 'Алиса и Вася, большое спасибо за теплый приём!'
     name1 = feedback[:5]
     name2 = feedback[8:12]
     print(f'Назначить премию: {name1}/{name2}')
-#7
+
+    # 7.
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     number = int(input("Введите номер буквы английского алфавита: "))
-    letter_index = (number - 1) % len(alphabet)
-    letter = alphabet[letter_index]
-    print(letter, end="")
-    for i in range(1, 4):
-        next_letter_index = (letter_index + i) % len(alphabet)
-        print(alphabet[next_letter_index], end="")
-    print()
-#8
+    if 1 <= number <= len(alphabet):
+        letter_index = number - 1
+        print(alphabet[letter_index:letter_index + 4])
+    else:
+        print("Введите номер от 1 до 26.")
+
+    # 8.
     my_list = []
     print("Созданный список:", my_list)
     my_list.append(1)
@@ -64,15 +68,17 @@ def main():
     two_dimensional_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     print("Двумерный список:", two_dimensional_list)
     element = two_dimensional_list[1][2]
-    print("Элемент на позиции [1][2]:", element)
+    print("Элемент на позиции [1] [2] :", element)
     my_list.clear()
     print("Список после очистки:", my_list)
-#9
+
+    # 9.
     empty_tuple = ()
     print("Пустой кортеж:", empty_tuple)
     filled_tuple = (1, 2, 3, 'a', 'b', 'c')
     print("Заполненный кортеж:", filled_tuple)
-#10
+
+    # 10.
     empty_set = set()
     print("Пустое множество:", empty_set)
     filled_set = {1, 2, 3, 'a', 'b', 'c'}
@@ -91,7 +97,8 @@ def main():
     print("Разность (set_a - set_b):", difference_set)
     symmetric_difference_set = set_a ^ set_b
     print("Симметричная разность:", symmetric_difference_set)
-#11
+
+    # 11.
     empty_dict = {}
     print("Пустой словарь:", empty_dict)
     filled_dict = {
@@ -106,6 +113,7 @@ def main():
     print("Словарь после удаления значения:", filled_dict)
     filled_dict['city'] = 'Saint Petersburg'
     print("Словарь после изменения значения:", filled_dict)
+
 
 if __name__ == "__main__":
     main()
