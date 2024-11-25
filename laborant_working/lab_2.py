c68a04ae0fb9b3ac69813dfc134abd90cee5fa3f
melvinbb <141237291+melvinbb@users.noreply.github.com>
2024-11-25 12:39:32 +0900

    Update lab_2.py

--- laborant_working/lab_2.py
+++ laborant_working/lab_2.py
@@ -1,41 +1,38 @@
 def main():
-
-#1
+    # 1
     input_string = input("Введите строку: ")
     if input_string == "Python":
         print("ДА")
     else:
         print("НЕТ")
-#2
+
+    # 2
     string1 = input("Введите первую строку: ")
     string2 = input("Введите вторую строку: ")
-
     valid_strings = ["да", "нет"]
-
     if (string1 in valid_strings) and (string2 in valid_strings):
         print("ВЕРНО")
     else:
         print("НЕВЕРНО")
-#3
-    one = input()
-    two = input()
-    three = input()
 
-    if one in ('1', 'раз') and two in ('2', 'два') and three in ('3', 'три'):
+    # 3
+    one, two, three = input(), input(), input()
+    if one == 'раз' and two == 'два' and three == 'три':
+        print('ГОРИ')
+    elif one == '1' and two == '2' and three == '3':
         print('ГОРИ')
     else:
         print('НЕ ГОРИ')
 
-#4
-    city_1 = input()
-    city_2 = input()
-
-    if (city_1 == 'Тула' or city_1 == 'Пенза' or city_2 == 'Тула' or city_2 == 'Пенза') and city_1 != city_2:
-      print('ДА')
+    # 4
+    city1 = input("Введите город:")
+    city2 = input("Введите город:")
+    if city1 == "Тула" or city2 == "Пенза":
+        print("ДА")
     else:
-      print('НЕТ')
+        print("НЕТ")
 
-#5
+    # 5
     n = int(input("Введите количество километров в марафоне (n): "))
     m = int(input("Введите расстояние, которое спортсмен пробегает за день (m): "))
 
@@ -45,7 +42,7 @@ def main():
         days = (n // m) + 1
 
     print("Спортсмен добежит до финиша на день:", days)
-#6
+    # 6
     number = int(input())
 
     first_digit = number // 100
@@ -58,7 +55,7 @@ def main():
         print("Подходит")
     else:
         print(sum_of_digits, middle_digit)
-#7
+    # 7
     category = input("Категория: ")
 
     if category.lower() == "продукты":
@@ -71,44 +68,46 @@ def main():
             print("Попробуйте экзотические фрукты!")
     else:
         print("Загляните в товары для дома!")
-#8
 
+    # 8
     prices_str = input("Введите цены через пробел: ").split()
     prices = [float(p) for p in prices_str]
 
     if prices == sorted(prices):
-      total = sum(prices) / 2
-      print("Акция!\nК оплате:", f"{total:.2f}")
+        total = sum(prices) / 2
+        print("Акция!\nК оплате:", f"{total:.2f}")
     elif prices == sorted(prices, reverse=True):
-      total = sum(prices) / 3
-      print("Акция!\nК оплате:", f"{total:.2f}")
+        total = sum(prices) / 3
+        print("Акция!\nК оплате:", f"{total:.2f}")
     else:
-      total = sum(prices)
-      print("К оплате:", f"{total:.2f}")
+        total = sum(prices)
+        print("К оплате:", f"{total:.2f}")
 
-#9
-    n1 = int(input("Введите количество посетителей вчера: "))
-    n2 = int(input("Введите количество посетителей позавчера: "))
+    # 9
+    n1 = int(input())
+    n2 = int(input())
+    if n1 < n2:
+        print(f'Сегодня магазин посетит: {n2 + (n2 - n1)} человек')
+    elif n1 > n2:
+        print(f'Сегодня магазин посетит: {n2 - (n2 - n1)} человек')
+    elif n1 == n2:
+        print(f'Сегодня магазин посетит: {n2} человек')
 
-    visitors = n2 + abs(n1 - n2)
-
-    print(f'Сегодня магазин посетит: {visitors} человек')
-
-
-#10
+    # 10
     year = int(input("Введите год: "))
 
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print(year, "високосный")
     else:
         print(year, "не високосный")
-#11
+    # 11
     number = int(input("Введите целое число: "))
 
     if number % 2 == 0:
         print(number, " - четное число")
     else:
         print(number, " - нечетное число")
+
 
 if __name__ == "__main__":
     main()
