efe2a2277f27b2a1d65015370bf36cfc06a920e2
melvinbb <141237291+melvinbb@users.noreply.github.com>
2025-01-17 12:49:12 +0900

    Create lab4.py

---
+++ laborant_working2/lab4.py
@@ -0,0 +1,21 @@
+def horse2(a: str):
+    stolbec = a[:1]
+    stroka = a[1:]
+    stbs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    strs = ['1', '2', '3', '4', '5', '6', '7', '8']
+    n1 = stbs.index(stolbec)
+    n2 = strs.index(stroka)
+
+    tmp = [(n1 - 2, n2 + 1), (n1 - 2, n2 - 1), (n1 - 1, n2 + 2),
+           (n1 - 1, n2 - 2), (n1 + 1, n2 + 2), (n1 + 1, n2 - 2),
+           (n1 + 2, n2 + 1), (n1 + 2, n2 - 1)]
+
+    res = []
+    for (a, b) in tmp:
+        if (a >= 0) and (b >= 0) and (a <= 8) and (b <= 8):
+            res += [f'{stbs[a]}{strs[b]}']
+    print(*sorted(res), sep='\n')
+
+
+if __name__ == '__main__':
+    horse2(input())
